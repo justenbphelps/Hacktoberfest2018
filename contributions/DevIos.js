ef95990697f7d8d43d37143a742dbbb18c6e23de
Leon Koop <leonkoop06@gmail.com>
2019-10-07 20:44:14 +0200

    Added my story

---
+++ contributions/DevIos.js
@@ -0,0 +1,10 @@
+const story = {
+  name: "Shubham",
+  location: "Stockholm - Sweden",
+  age: "16",
+  why: "I like to take new challenges everyday about something that I didn't know yet"
+}
+
+const me = `Hey, I'm ${story.name}, I'm living in ${story.location} and I'm ${story.age} years old.\nI became a developer, because ${story.why}.`;
+
+console.log(me);
