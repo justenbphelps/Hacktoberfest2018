51b472ab3b6ad4abe96373375f15c990f2d1c7eb
Dirk Soentjens <dirk.soentjens@trivago.com>
2019-10-08 11:44:31 +0200

    init commit

---
+++ contributions/dirk-soentjens.js
@@ -0,0 +1,6 @@
+let first_name = "Dirk";
+let last_name = "Soentjens";
+
+(function () {
+    console.log(`Hi! my name is ${first_name} ${last_name} . I like to code in Javascript and I currently learning Python.`);
+})();
