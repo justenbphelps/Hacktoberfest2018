bd96e5bcc668e2fb700efd40cad1010f6781d58d
Prerna Puri <54809133+TechnoGuy0306@users.noreply.github.com>
2019-10-10 21:52:55 +0530

    Create TechnoGuy0306.cpp

---
+++ contributions/TechnoGuy0306.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"Hi I am Prerna aka TechnoGuy";
+cout<<"I am an aspiring programmer";
+cout<<"And I wish to develop my problem solving skills";
+return 0;
+}
