b3799cb94867a758f1efbbd5d19867f8f7be653a
4rturd13 <4rturd13@gmail.com>
2019-10-12 18:10:16 -0500

    4rturd13

---
+++ contributions/4rturd13.js
@@ -0,0 +1,9 @@
+let mySelf = {
+    "name": "Arturo",
+    "age": 31,
+    "city": "Valledupar",
+    "country": "Colombia",
+    "history": "As a child I liked videogames and caught all my concentration. This was fine for me because I have trouble concentrating thoroughly on something. I have always liked computers but, but in my city, nobody programmed because what I didn't know was to do this. It was until I changed the city that I learned about code and it captured my attention as much as video games. That's how my passion for programming was born and I'm about to be a full stack developer in Javascript"
+}
+
+console.log(`Hi, i'm ${mySelf.name}, i'm ${mySelf.age}, i live in ${mySelf.city} and ${mySelf.history}`)
