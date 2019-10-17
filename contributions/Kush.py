30ec7e9606af13c1b9f1cce6cb91dee604e04208
kush1307 <35497196+kush1307@users.noreply.github.com>
2019-10-17 22:50:11 +0530

    new Kush.py added

---
+++ contributions/Kush.py
@@ -0,0 +1,5 @@
+name = "Kush"
+desig = "Aspiring Data Scientist"
+age = 20
+txt = "My name is {} and i am {} . Currently i am a student also im a highly motivated {}" 
+print(txt.format(name , age , desig))
